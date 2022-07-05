ff9fd61dac2ea87bb25b6e12e6f268cc64ca03de
patchcablemgr <53905413+patchcablemgr@users.noreply.github.com>
2022-07-05 07:30:13 -0700

    Update app_init.sh

--- app_init/app_init.sh
+++ app_init/app_init.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/usr/local/bin/wait-for-it.sh database:3306 --timeout=30 --strict -- /usr/local/bin/db_init.sh
+/usr/local/bin/wait-for-it.sh database:3306 --timeout=0 --strict -- /usr/local/bin/db_init.sh
